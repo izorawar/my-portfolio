1ed2d8690175e00fbe49692f3737d8d5f5bd68e7
izorawar <sin11529@sheridancollege.ca>
2023-01-01 17:46:30 -0500

    fixed escape apostrophe and disabled ESLint warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Jorawar's Portfolio</title>
+        <title>{" Jorawar's Portfolio "}</title>
       </Head>
       
       <Header />
